eed069e1779f217bab3e4a011b3c13d024e57a67
Steven Harradine <stevenharradine@gmail.com>
2015-11-30 15:39:15 -0500

    add owncloud config.php and add external ip to defaults

---
+++ templates/owncloud/config/config.php
@@ -0,0 +1,22 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc3aspnwu25x',
+  'passwordsalt' => 'RNTrvCh2gJ3ds0Vc+ixx/HbLq9g8dw',
+  'secret' => 'Pre3Pq82QsOVWfdMxCgx7PuGXpDkNBicKrzP7iYqKc5D43eZ',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+    1 => 'owncloud_external_ip',
+  ),
+  'datadirectory' => '/data/www/docroot/owncloud/data',
+  'overwrite.cli.url' => 'http://localhost',
+  'dbtype' => 'mysql',
+  'version' => '8.2.1.4',
+  'dbname' => '{{ project }}',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => '{{ project }}',
+  'dbpassword' => 'secure_password',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
